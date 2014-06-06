49b904f33e010a73eafeb5f7c774c19408c7d43e
kyrie91 <artoria91@gmail.com>
2014-06-06 12:08:48 +0800

    update

--- lrucache/lru_cache.cpp
+++ lrucache/lru_cache.cpp
@@ -2,112 +2,155 @@
 #include <string.h>
 #include <stdint.h>
 
-ClruCache::ClruCache(int size, int value_len)
-{
-    max_ele_num = ((size >= MIN_BUCKET_SIZE) ? size:MIN_BUCKET_SIZE);
-    now_ele_num = 0;
-    max_value_size = ((value_len > DEFAULT_BUCKET_SIZE) ? value_len:DEFAULT_BUCKET_SIZE);
-    INIT_LIST_HEAD(&cache_head);
-    mapCache.clear();
-}
-
-ClruCache::~ClruCache()
-{
-    mapCache.clear();
-    struct list_head *pl;
-    struct list_head *tmp;
-
-    list_for_each_safe(pl, tmp, &cache_head)
+static int GetMinPrim(int limit)
+{					
+	if (limit % 2 == 0)								///判断奇偶
+	{
+		limit++;
+	}
+	
+	for (int i = limit; ; i += 2)
     {
-        struct CacheData* pc = list_entry(pl, CacheData, list);
-        ///释放内存
-        free(pc);
-    }
-}
-
-///确保max_ele_size是一个大于2的值
-int ClruCache::Insert(long key, const std::string& data, long expire_time, int option)
-{
-    if (data.size() > (uint32_t)max_value_size)
-        return E_VALUE_TOO_LARGE;
-
-    ///检查是否已存在
-    __gnu_cxx::hash_map<long, struct CacheData*>::iterator it;
-    it = mapCache.find(key);
-    if (it == mapCache.end())                                                                       ///没找到
-    {
-        if (now_ele_num >= max_ele_num)
+        int cnt = 0;
+        for (int j = 2; j*j <= i; ++j)
         {
-            struct CacheData* tail = list_entry(&cache_head, CacheData, list);                      ///根据双向循环链表的头找到tail
-            mapCache.erase(tail->key);                                                              ///从hash_map里面删除尾节点对应的key
-            list_del(&(tail->list));                                                                ///从双向循环链表里面删除尾节点
-            free(tail);                                                                             ///delete尾节点
-            --now_ele_num;                                                                          ///总的节点数量减一
+             if ((i % j) == 0)
+             {
+                 cnt++;
+                 break;
+             }
         }
 
-        struct CacheData *pc = (struct CacheData *)malloc(sizeof(struct CacheData) + max_value_size);
-        pc->key = key;
-        pc->expire_time = expire_time;
-        pc->insert_time = time(NULL);
-        pc->data_len = data.size();
-        memcpy(pc->data, data.c_str(), pc->data_len);
-        list_add(&(pc->list), &cache_head);
-
-        mapCache[key] = pc;
-        ++now_ele_num;
-    }
-    else                                                                                            ///找到了
-    {
-        CacheData* pc = (it->second);                                                               ///找到对用的cache_data指针
-        if (((pc->expire_time <= 0) || (pc->expire_time + pc->insert_time >= time(NULL)))
-                && (option == FAIL_IF_EXIST)) {                                                     ///没过期，并且选项是FAIL_IF_EXIST
-            return E_ALREADY_EXSIT;
-        }
-        else
+        if (cnt == 0)
         {
-            list_move(&(pc->list), &cache_head);                                                    ///重要：将其移到链表头，LRU
-            pc->expire_time = expire_time;
-            pc->insert_time = time(NULL);
-            pc->data_len = data.size();
-            memcpy(pc->data, data.c_str(), pc->data_len);                                           ///data_len不会超过申请的内存
+			return i;
         }
     }
-
-    return 0;
+	
+	return limit;
 }
 
-int ClruCache::Get(long key, std::string& data)
+namespace KYRIE_UTILS
 {
-    __gnu_cxx::hash_map<long, struct CacheData*>::iterator it;
-    it = mapCache.find(key);
-    if (it == mapCache.end())                                                                       ///没找到
-        return E_NOT_EXIST;
+	ClruCache::ClruCache(int size, int key_len)
+	{
+		max_ele_num = ((size >= MIN_BUCKET_SIZE) ? size:MIN_BUCKET_SIZE);
+		now_ele_num = 0;
+		max_key_size = ((key_len > DEFAULT_BUCKET_SIZE) ? key_len:DEFAULT_BUCKET_SIZE);
+		INIT_LIST_HEAD(&cache_head);
+		
+		mapCache.clear();
+		int min_prim = GetMinPrim(max_ele_num);															///获取大于元素个数的最小质数
+		mapCache.resize(min_prim);
+	}
 
-    struct CacheData* pc = (it->second);
-    if ((pc->expire_time > 0)
-            && (pc->expire_time + pc->insert_time < time(NULL))) {
-        return E_EXPIRED;
-    }
+	ClruCache::~ClruCache()
+	{
+		mapCache.clear();
+		struct list_head *pl;
+		struct list_head *tmp;
+		
+		list_for_each_safe(pl, tmp, &cache_head)
+		{
+			struct CacheData* pc = list_entry(pl, CacheData, list);
+			///释放内存
+			free(pc);
+		}
+	}
+	
+	///确保max_ele_size是一个大于2的值
+	int ClruCache::Insert(const std::string& key, int64_t data, long expire_time, int option)
+	{
+		if (key.size() > (uint32_t)max_key_size)
+			return E_LRU_KEY_TOO_LARGE;
 
-    data.resize(pc->data_len);
-    memcpy((char *)data.c_str(), pc->data, data.size());
-    list_move(&(pc->list), &cache_head);                                                            ///重要：将其移到链表头，LRU
+		///检查是否已存在
+		map_cache_data_t::iterator it;
+		it = mapCache.find(key.c_str());
+		if (it == mapCache.end())																		///没找到
+		{
+			if (now_ele_num >= max_ele_num)
+			{
+				struct CacheData* tail = list_entry(cache_head.prev, CacheData, list);
+				mapCache.erase(tail->key);																///删除旧key的hash_map记录
 
-    return 0;
-}
-
-int ClruCache::Delete(long key)
-{
-    __gnu_cxx::hash_map<long, struct CacheData*>::iterator it;
-    it = mapCache.find(key);
-    if (it == mapCache.end())                                                                       ///没找到
-        return E_NOT_EXIST;
-
-    struct CacheData *pc = (it->second);
-    mapCache.erase(key);                                                                            ///从hash_map里面删除尾节点对应的key
-    list_del(&(pc->list));                                                                          ///从双向循环链表里面删除尾节点
-    free(pc);                                                                                       ///delete尾节点
-    --now_ele_num;                                                                                  ///总的节点数量减一
-
-    return 0;
+				tail->data = data;																		///重新赋值
+				tail->expire_time = expire_time;
+				tail->insert_time = time(NULL);
+				tail->key_len = key.size();
+				memcpy(tail->key, key.c_str(), tail->key_len);
+				tail->key[tail->key_len] = '\0';
+				list_move(&(tail->list), &cache_head);													///重要：将其移到链表头，LRU
+				mapCache[tail->key] = tail;																///在hash_map中记录新key
+			}
+			else
+			{
+				struct CacheData *pc = (struct CacheData *)malloc(sizeof(struct CacheData) + max_key_size + 1);
+				pc->data = data;
+				pc->expire_time = expire_time;
+				pc->insert_time = time(NULL);
+				pc->key_len = key.size();
+				memcpy(pc->key, key.c_str(), pc->key_len);
+				pc->key[pc->key_len] = '\0';
+				list_add(&(pc->list), &cache_head);
+		
+				mapCache[pc->key] = pc;																	///重要，使用pc->key而不是传入的key
+				++now_ele_num;
+			}
+		}
+		else																							///找到了
+		{
+			CacheData* pc = (it->second);																///找到对用的cache_data指针
+			if (((pc->expire_time <= 0) || (pc->expire_time + pc->insert_time >= time(NULL))) 
+					&& (option == FAIL_IF_EXIST)) {														///没过期，并且选项是FAIL_IF_EXIST
+				return E_LRU_ALREADY_EXSIT;
+			}
+			else
+			{
+				list_move(&(pc->list), &cache_head);													///重要：将其移到链表头，LRU
+				pc->expire_time = expire_time;
+				pc->insert_time = time(NULL);
+				pc->key_len = key.size();
+				memcpy(pc->key, key.c_str(), pc->key_len);												///data_len不会超过申请的内存
+				pc->key[pc->key_len] = '\0';
+			}
+		}
+	
+		return 0;
+	}
+	
+	int ClruCache::Get(std::string& key, int64_t& data)
+	{
+		map_cache_data_t::iterator it;
+		it = mapCache.find(key.c_str());
+		if (it == mapCache.end())																		///没找到
+			return E_LRU_NOT_EXIST;
+		
+		struct CacheData* pc = (it->second);
+		if ((pc->expire_time > 0) 
+				&& (pc->expire_time + pc->insert_time < time(NULL))) {
+			return E_LRU_EXPIRED;
+		}
+	
+		data = pc->data;
+		list_move(&(pc->list), &cache_head);															///重要：将其移到链表头，LRU
+	
+		return 0;	
+	}
+	
+	int ClruCache::Delete(std::string& key)
+	{
+		map_cache_data_t::iterator it;
+		it = mapCache.find(key.c_str());
+		if (it == mapCache.end())																		///没找到
+			return E_LRU_NOT_EXIST;
+		
+		struct CacheData *pc = (it->second);
+		mapCache.erase(key.c_str());																	///从hash_map里面删除尾节点对应的key
+		list_del(&(pc->list));																			///从双向循环链表里面删除尾节点
+		free(pc);																						///delete尾节点
+		--now_ele_num;																					///总的节点数量减一
+	
+		return 0;
+	}
 }
--- lrucache/lru_cache.h
+++ lrucache/lru_cache.h
@@ -4,52 +4,67 @@
 #include "list.h"
 #include <ext/hash_map>
 
-#define MIN_BUCKET_SIZE         128
-#define DEFAULT_BUCKET_SIZE     4
+#define MIN_BUCKET_SIZE			128
+#define DEFAULT_BUCKET_SIZE		64
 
-enum INSERT_OPTION
+namespace KYRIE_UTILS
 {
-    FAIL_IF_EXIST   = 0,
-    OVERWRITE       = 1,
-};
+	enum INSERT_OPTION
+	{
+		FAIL_IF_EXIST	= 0,
+		OVERWRITE		= 1,
+	};
+	
+	enum ENUM_ERRCODE
+	{
+		E_LRU_OK				= 0,
+		E_LRU_NOT_EXIST			= 1,
+		E_LRU_ALREADY_EXSIT		= 2,
+		E_LRU_EXPIRED			= 3,
+		E_LRU_KEY_TOO_LARGE		= 4,
+	};
 
-enum ENUM_ERRCODE
-{
-    E_OK                = 0,
-    E_NOT_EXIST         = 1,
-    E_ALREADY_EXSIT     = 2,
-    E_EXPIRED           = 3,
-    E_VALUE_TOO_LARGE   = 4,
-};
+	struct CacheData					///定制化的lru cachedata, map<string, int64_t>
+	{
+		struct list_head list;
+		int64_t data;
+		long insert_time;
+		long expire_time;
+		int total_len;
+		short key_len;
+		char key[0];					///占位符，gcc支持
+	};
 
-struct CacheData
-{
-    struct list_head list;
-    long key;
-    long insert_time;
-    long expire_time;
-    int total_len;
-    int data_len;
-    char data[0];                   ///占位符，gcc支持
-};
+	template <class _Tp>
+	struct str_equal_to
+	{
+		bool  
+		operator()(const _Tp& __x, const _Tp& __y) const  
+		{ return strcmp( __x, __y ) == 0; }  
+	};
 
-class ClruCache
-{
-    public:
-        ClruCache(int ele_size, int value_len);
-        ~ClruCache();
-
-    public:
-        int Insert(long key, const std::string& data, long expire_time, int option);
-        int Get(long key, std::string& data);
-        int Delete(long key);
-
-    private:
-        struct list_head cache_head;
-        __gnu_cxx::hash_map<long, struct CacheData*> mapCache;
-        int max_ele_num;
-        int now_ele_num;
-        int max_value_size;             ///<k, v>对里面，value的最大长度，可根据需要调整，为了减少free & malloc将value拼在节点数据的后面
-};
+	typedef __gnu_cxx::hash_map<const char*, 
+								struct CacheData*, 
+								__gnu_cxx::hash<const char*>, 
+								str_equal_to<const char*> > map_cache_data_t;
+	class ClruCache
+	{
+		public:
+			ClruCache(int ele_size, int key_len);
+			~ClruCache();
+			
+		public:
+			int Insert(const std::string& key, int64_t data, long expire_time, int option);
+			int Get(std::string& key, int64_t& data);
+			int Delete(std::string& key);
+			
+		private:
+			struct list_head cache_head;
+			map_cache_data_t mapCache;
+			int max_ele_num;
+			int now_ele_num;
+			int max_key_size;				///<k, v>对里面，key的最大长度，可根据需要调整，为了减少free & malloc将key拼在节点数据的后面
+	};
+}
 
 #endif
